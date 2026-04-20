4c5dc24c57f7fa1b7a13f73f9309274ce58357d2
adamjbradley <adam_j_bradley@yahoo.com>
2026-04-20 12:19:55 +1000

    feat(logic-core): add ReaderTrustStoreUpdater.parsePem

--- Modules/logic-core/Tests/Placeholder.swift
+++
@@ -1,8 +0,0 @@
-import XCTest
-@testable import logic_core
-
-final class PlaceholderTests: XCTestCase {
-  func testPackageBuilds() {
-    XCTAssertTrue(true)
-  }
-}
---
+++ Modules/logic-core/Tests/ReaderTrustStoreUpdaterTests.swift
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2025 European Commission
+ *
+ * Licensed under the EUPL, Version 1.2 or - as soon they will be approved by the European
+ * Commission - subsequent versions of the EUPL (the "Licence"); You may not use this work
+ * except in compliance with the Licence.
+ *
+ * You may obtain a copy of the Licence at:
+ * https://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the Licence is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF
+ * ANY KIND, either express or implied. See the Licence for the specific language
+ * governing permissions and limitations under the Licence.
+ */
+import XCTest
+@testable import logic_core
+
+final class ReaderTrustStoreUpdaterTests: XCTestCase {
+
+  static let pemBlockA = """
+    -----BEGIN CERTIFICATE-----
+    MIIBnzCCAUagAwIBAgIUQSg5NhDlxwDFyAM7YJe++0QGyKIwCgYIKoZIzj0EAwIw
+    KTEnMCUGA1UEAwwedmVyaWZpZXIyLnRoZWF1c3RyYWxpYWhhY2suY29tMB4XDTI2
+    MDIwMzAzNTIwM1oXDTI3MDIwMzAzNTIwM1owKTEnMCUGA1UEAwwedmVyaWZpZXIy
+    LnRoZWF1c3RyYWxpYWhhY2suY29tMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAE
+    1Z2eGpdQVfWkAQQmNv8oT+lMwbhsFxWTZmhAYFHR5wa29fr30LbWoYQyrvOqOkSG
+    rLaQ9PwoiedvGUtnN5Jd2qNMMEowKQYDVR0RBCIwIIIedmVyaWZpZXIyLnRoZWF1
+    c3RyYWxpYWhhY2suY29tMB0GA1UdDgQWBBRt0uKz8aKVlUxKF9j6vhAsGl3nHDAK
+    BggqhkjOPQQDAgNHADBEAiAQ+AlF3Q4dput8QTizDyKo99R/sv3CC7BzqEjOxxsn
+    zQIgF+rnBf0HghobWkjSVNwP8j/ekasfjp+1HDJclcNaUvs=
+    -----END CERTIFICATE-----
+    """
+
+  static let pemBlockB = """
+    -----BEGIN CERTIFICATE-----
+    MIIBkzCCATqgAwIBAgIUFO2gZtA0OguCDhsJ/o1d64wPcWAwCgYIKoZIzj0EAwIw
+    KDEmMCQGA1UEAwwdcnAudGhlYXVzdHJhbGlhaGFjay5jb20gUlAgQ0EwHhcNMjYw
+    NDE3MDc1NzAxWhcNMjcwNDE3MDc1NzAxWjAoMSYwJAYDVQQDDB1ycC50aGVhdXN0
+    cmFsaWFoYWNrLmNvbSBSUCBDQTBZMBMGByqGSM49AgEGCCqGSM49AwEHA0IABAtG
+    NbR+9219/Ej/K7rfdEwl1sxdlVWJhMd5Hps3ml+KfBnaMF3t/kcLrOfJFGDm7mQc
+    91NgcpLp0CNKAbnjYvujQjBAMA8GA1UdEwEB/wQFMAMBAf8wDgYDVR0PAQH/BAQD
+    AgEGMB0GA1UdDgQWBBRwBkmZZjR3i0qRWN7Ii0vEFwV6OjAKBggqhkjOPQQDAgNH
+    ADBEAiBeMJ3E+uQBkOY6BQ/D1h1vGRoIznr25Vwy+31jcWA27wIgV4PrQ9J7UEXc
+    fhpKRBn3CaN+z3oStgcE6PpIyAGdXWU=
+    -----END CERTIFICATE-----
+    """
+
+  func testParsePemWithTwoBlocksReturnsTwoDerChunks() {
+    let bundle = Self.pemBlockA + "\n" + Self.pemBlockB
+    let ders = ReaderTrustStoreUpdater.parsePem(bundle)
+    XCTAssertEqual(ders.count, 2)
+    XCTAssertGreaterThan(ders[0].count, 50)
+    XCTAssertGreaterThan(ders[1].count, 50)
+    XCTAssertNotEqual(ders[0], ders[1])
+  }
+}
